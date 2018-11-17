144d424727e6c682e34885bb09e52d65e9882153
sakshamahluwalia <saksham.ahluwalia@mail.utoronto.ca>
2018-11-17 18:10:45 -0500

    intial commit

---
+++ table_definitions/q1.sql
@@ -0,0 +1,30 @@
+-- VoteRange
+
+SET SEARCH_PATH TO parlgov;
+drop table if exists q1 cascade;
+
+-- You must not change this table definition.
+
+create table q1(
+year INT,
+countryName VARCHAR(50),
+voteRange VARCHAR(20),
+partyName VARCHAR(100)
+);
+
+
+-- You may find it convenient to do this for each of the views
+-- that define your intermediate steps.  (But give them better names!)
+DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS part1 CASCADE;
+
+-- Define views for your intermediate steps here.
+create view part1 as
+	select *
+	from election join election_result
+			on election.id = election_result.id
+	where extract(year from election.e_date) >= 1996 and extract(year from election.e_date) <= 2016
+
+-- the answer to the query 
+insert into q1 
+
