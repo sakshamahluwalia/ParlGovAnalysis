400c49a07dc74ce7e488c3f327ae7e853740e4f3
Ahluwalia Saksham <ahluwa41@teach.cs.toronto.edu>
2018-11-19 14:49:58 -0500

    finished q1

--- table_definitions/q1.sql
+++ table_definitions/q1.sql
@@ -15,25 +15,56 @@ partyName VARCHAR(100)
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS votes_validNull, votes_validNotNullCalculated, votes_validNotNullFromStart, votes_validFinal CASCADE;
+DROP VIEW IF EXISTS VoteForParty CASCADE;
 DROP VIEW IF EXISTS part1 CASCADE;
 DROP VIEW IF EXISTS part2a CASCADE;
 DROP VIEW IF EXISTS part2b CASCADE;
 DROP VIEW IF EXISTS ranges CASCADE;
 DROP VIEW IF EXISTS answer CASCADE;
 
--- Define views for your intermediate steps here.
+-- get the electionId, country_id and the date where votes_valid is not given from election.
+create view votes_validNull as
+	select id, country_id, e_date
+	from election
+	where votes_valid is null;
 
+-- Calculate the votes_valid for every election in, 'votes_validNull.'
+create view votes_validNotNullCalculated as 
+	SELECT vvn.id, vvn.country_id, vvn.e_date, sum(votes) as votes_valid
+	FROM election_result, votes_validNull vvn
+	WHERE election_result.election_id = vvn.id
+	group by vvn.id, vvn.country_id, vvn.e_date;
+
+-- elections that do not have votes_valid as null from the start.
+create view votes_validNotNullFromStart as
+	select id, country_id, e_date, votes_valid
+	from election e
+	where votes_valid is not null;
+
+-- combine, 'votes_validNotNullCalculated' and 'votes_validNotNullFromStart' to get all elections with votes_valid as not null.
+create view votes_validFinal as
+	select * from votes_validNotNullCalculated calculated union select * from votes_validNotNullFromStart fromStart;
+
+-- election_results that account for the # of votes recieved by a party.
+create view VoteForParty as
+	select election_id, party_id, votes
+	from election_result
+	where votes is not null;
+
+-- get the vote % of each party from each country for each election from 1996 - 2016.
 create view part1 as 
-	select e_date, e.country_id, party_id, (election_result.votes * 1.0/votes_valid * 1.0) * 100.0 as vote_percentage 	
-	from election e join country on e.country_id = country.id join election_result on e.id = election_id join party on party_id = party.id	
+	select e_date, e.country_id, party_id, (VoteForParty.votes * 1.0/votes_valid * 1.0) * 100.0 as vote_percentage 	
+	from votes_validFinal e join country on e.country_id = country.id join VoteForParty on e.id = election_id join party on party_id = party.id	
 	where extract(year from e.e_date) >= '1996' and extract(year from e.e_date) <= '2016';
 
+-- get the avg vote % of each party from each country for all years.
 create view part2a as 
 	select extract(year from e_date) as year, country_id, party_id, avg(vote_percentage) as percentage	
 	from part1 		
 	group by extract(year from e_date), country_id, party_id;
 
+-- convert the percentages into vote ranges.
 create view ranges as 
 	select p2a.year as year, p2a.country_id, p.name_short as partyName, 
 		case 
